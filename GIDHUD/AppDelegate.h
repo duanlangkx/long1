6e84a63ade1f88df2b647ecb715c0292259bf603
DC004 <dc004@duanpanlong.lan>
2016-04-01 14:03:58 +0800

    Initial Commit

---
+++ GIDHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIDHUD
+//
+//  Created by DC004 on 16/4/1.
+//  Copyright © 2016年 DC004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
